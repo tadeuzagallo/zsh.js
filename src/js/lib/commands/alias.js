3e4962ddc52bd28d092e85e3087dc6202bab9a25
Tarcisio Gruppi <txgruppi@gmail.com>
2014-01-16 10:23:17 -0200

    Updated alias command to show defined aliases if called with no arguments

--- src/js/lib/commands/alias.js
+++ src/js/lib/commands/alias.js
@@ -3,6 +3,7 @@ var CommandManager = require('../command-manager');
 CommandManager.register('alias', alias);
 
 function alias(args, stdin, stdout, stderr) {
+  var buffer = '';
   if (args.arguments.length) {
     var key = args.arguments.shift();
     var index;
@@ -21,7 +22,13 @@ function alias(args, stdin, stdout, stderr) {
         CommandManager.alias(key, command);
       }
     }
+  } else {
+    var aliases = CommandManager.alias();
+
+    for (var i in aliases) {
+      buffer += i + "='" + aliases[i] + "'"
+    }
   }
 
-  stdout('');
+  stdout(buffer);
 }
