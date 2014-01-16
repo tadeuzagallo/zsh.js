3fc193e7c3ed85e1a573dfad42a3b803c539fa31
tadeuzagallo <tadeuzagallo@gmail.com>
2014-01-16 19:09:45 -0200

    restructure command-manager

--- src/js/lib/command-manager.js
+++ src/js/lib/command-manager.js
@@ -3,44 +3,50 @@ var ArgsParser = require('./args-parser');
 var CommandManager = {
   commands: {},
   aliases: {},
-  isValid: function (cmd) {
-    return !!(this.commands[cmd] || this.aliases[cmd]);
-  },
-  autoComplete: function (cmd) {
-    var matches = [];
+};
 
-    (Object.keys(this.commands).concat(Object.keys(this.aliases))).forEach(function (command) {
-      if (command.substr(0, cmd.length) === cmd) {
-        matches.push(command);
-      }
-    });
+CommandManager.isValid = function (cmd) {
+  return !!(this.commands[cmd] || this.aliases[cmd]);
+};
 
-    return matches;
-  },
-  exec: function (cmd, args, stdin, stdout, stderr, next) {
-    if (this.aliases[cmd]) {
-      var line = (this.aliases[cmd] + args).trim().split(' ');
-      return this.exec(line.shift(), line.join(' '), stdin, stdout, stderr, next);
+CommandManager.autoComplete = function (cmd) {
+  var matches = [];
+
+  (Object.keys(this.commands).concat(Object.keys(this.aliases))).forEach(function (command) {
+    if (command.substr(0, cmd.length) === cmd) {
+      matches.push(command);
     }
-    if (!this.commands[cmd]) {
-      stdout.write('zsh: command not found: ' + cmd);
-      next();
-    } else {
-      this.commands[cmd].call(undefined, ArgsParser.parse(args), stdin, stdout, stderr, next);
-    }
-  },
-  register: function (cmd, fn) {
-    this.commands[cmd] = fn;
-  },
-  alias: function (cmd, original) {
-    if (arguments.length === 0) {
-      return this.aliases;
-    }
-    this.aliases[cmd] = original;
-  },
-  get: function(cmd) {
-    return this.commands[cmd];
+  });
+
+  return matches;
+};
+
+CommandManager.exec = function (cmd, args, stdin, stdout, stderr, next) {
+  if (this.aliases[cmd]) {
+    var line = (this.aliases[cmd] + args).trim().split(' ');
+    return this.exec(line.shift(), line.join(' '), stdin, stdout, stderr, next);
   }
+  if (!this.commands[cmd]) {
+    stdout.write('zsh: command not found: ' + cmd);
+    next();
+  } else {
+    this.commands[cmd].call(undefined, ArgsParser.parse(args), stdin, stdout, stderr, next);
+  }
+};
+
+CommandManager.register = function (cmd, fn) {
+  this.commands[cmd] = fn;
+};
+
+CommandManager.alias = function (cmd, original) {
+  if (arguments.length === 0) {
+    return this.aliases;
+  }
+  this.aliases[cmd] = original;
+};
+
+CommandManager.get = function(cmd) {
+  return this.commands[cmd];
 };
 
 module.exports = CommandManager;
