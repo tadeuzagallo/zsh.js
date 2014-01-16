509704f2aaa9f7d998d8eeaa95b0dce73648f6bd
tadeuzagallo <tadeuzagallo@gmail.com>
2014-01-16 21:38:01 -0200

    ignoring auto-generated files

--- src/js/lib/commands.js
+++
@@ -1,7 +0,0 @@
-require("./commands/alias.js");
-require("./commands/cat.js");
-require("./commands/cd.js");
-require("./commands/echo.js");
-require("./commands/ls.js");
-require("./commands/pwd.js");
-require("./commands/site-helpers.js");
