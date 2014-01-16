b661ae30c23a1019470b16e7d1e36ba5f5495838
tadeuzagallo <tadeuzagallo@gmail.com>
2014-01-15 23:28:31 -0200

    testing translate vs realpath and translation of home(~)

--- spec/fs-spec.js
+++ spec/fs-spec.js
@@ -9,17 +9,34 @@ describe('file-system', function () {
     it('should open a file', function () {
       expect(fs.open('/Users/guest/about.md')).toEqual(fs.root.Users.guest['about.md']);
     });
+
+    it('should return null for unexistant file', function () {
+      expect(fs.open('~/foo.'));
+    });
   });
-  describe('realpath', function () {
+
+  describe('translatePath', function () {
+    it('should translate home', function () {
+      expect(fs.translatePath('~')).toEqual(fs.home);
+      expect(fs.translatePath('~/.')).toEqual(fs.home);
+      expect(fs.translatePath('~/..')).toEqual('/Users');
+    });
+
     it('should translate current path', function () {
-      expect(fs.realpath('.')).toEqual(fs.currentPath);
+      expect(fs.translatePath('.')).toEqual(fs.currentPath);
     });
 
     it('should translate parent path', function () {
       fs.currentPath = '/Users/guest';
-      expect(fs.realpath('..')).toEqual('/Users');
+      expect(fs.translatePath('..')).toEqual('/Users');
     });
 
+    it('should not validate a path', function () {
+      expect(fs.translatePath('./foo')).toEqual(fs.home + '/foo');
+    });
+  });
+
+  describe('realpath', function () {
     it('should validate a path', function () {
       expect(fs.realpath('./foo')).toBeNull();
     });
