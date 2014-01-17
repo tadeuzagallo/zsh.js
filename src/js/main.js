e73b15476cf5d8a8d2013acdab6612df336d1fff
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-01-17 15:53:06 -0200

    wait for DOM ready

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,7 @@ require('./lib/commands');
 var Terminal = require('./lib/terminal');
 var Tmux = require('./lib/tmux');
 
-Terminal.init(document.getElementById('content'), document.getElementById('status-bar'));
-Tmux.init(Terminal);
+document.addEventListener('DOMContentLoaded', function (e) {
+  Terminal.init(document.getElementById('content'), document.getElementById('status-bar'));
+  Tmux.init(Terminal);
+});
--- src/js/site.js
+++ src/js/site.js
@@ -1,25 +1,26 @@
 require('./main');
 
-var _onkeydown = window.onkeydown;
-var p = function () {};
+document.addEventListener('DOMContentLoaded', function (e) {
+  var _onkeydown = window.onkeydown;
+  var p = function () {};
 
-var codesFromString = function(string) {
-  return [].map.call(string.toUpperCase(), function (a) {
-    return a.charCodeAt(0);
-  });
-};
+  var codesFromString = function(string) {
+    return [].map.call(string.toUpperCase(), function (a) {
+      return a.charCodeAt(0);
+    });
+  };
 
-var codes = codesFromString('about').concat([13]).concat(codesFromString('help'));
-var i = 0;
+  var codes = codesFromString('about').concat([13]).concat(codesFromString('help'));
+  var i = 0;
 
-var interval = setInterval(function () {
-  _onkeydown({ preventDefault: p, keyCode: codes[i++] });
+  var interval = setInterval(function () {
+    _onkeydown({ preventDefault: p, keyCode: codes[i++] });
 
-  if (i >= codes.length) {
-    clearInterval(interval);
-    window.onkeydown = _onkeydown;
-  } else {
-    window.onkeydown = null;
-  }
-}, 200);
-
+    if (i >= codes.length) {
+      clearInterval(interval);
+      window.onkeydown = _onkeydown;
+    } else {
+      window.onkeydown = null;
+    }
+  }, 200);
+});
