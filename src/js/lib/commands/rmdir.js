a288051f940ff72026f6895d80b4feeea3ff6ae0
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-01-22 09:40:21 -0200

    add rmdir

---
+++ src/js/lib/commands/rmdir.js
@@ -0,0 +1,26 @@
+var CommandManager = require('../command-manager');
+var FS = require('../fs');
+
+CommandManager.register('rmdir', rmdir);
+
+function rmdir(args, stdin, stdout, stderr, next) {
+  args.arguments.forEach(function (arg) {
+    var path = arg.split('/');
+    var dirName = path.pop();
+    path = path.join('/');
+    var dir = FS.open(path);
+
+    if (!dir) {
+      stderr.write(FS.notFound('rmdir', arg));
+    } else if (typeof(dir) !== 'object' || typeof(dir[dirName]) !== 'object'){
+      stderr.write(FS.error('rmdir', arg, 'Not a directory'));
+    } else if (!dir[dirName]) {
+      stderr.write(FS.notFound('rmdir', arg));
+    } else {
+      delete dir[dirName];
+    }
+  });
+
+  FS.writeFS();
+  next();
+}
