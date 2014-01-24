d9c273deb4ed92b1c8ec9b5ee5d8c510c9e86e82
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-01-24 14:43:51 -0200

    fix: use args-parser on echo

--- src/js/lib/commands/echo.js
+++ src/js/lib/commands/echo.js
@@ -1,7 +1,13 @@
 var CommandManager = require('../command-manager');
+var ArgsParser = require('../args-parser');
 
 function echo(args, stdin, stdout, stderr, next) {
-  stdout.write(args.raw);
+  try {
+    stdout.write(ArgsParser.parseStrings(args.raw).join(' '));
+  } catch (err) {
+    stderr.write('zsh: ' + err.message);
+  }
+  
   next();
 }
 
