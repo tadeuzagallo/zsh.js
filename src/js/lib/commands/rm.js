0db445e25b21ae8a110b2607daf5ca294f30c667
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-01-23 23:53:58 -0200

    wip: add rm - missing arguments

---
+++ src/js/lib/commands/rm.js
@@ -0,0 +1,28 @@
+var CommandManager = require('../command-manager');
+var FS = require('../fs');
+
+CommandManager.register('rm', rm);
+
+function rm(args, stdin, stdout, stderr, next) {
+  args.arguments.forEach(function (arg) {
+    var path = arg.split('/');
+    var fileName = path.pop();
+    path = path.join('/');
+    var dir = FS.open(path);
+
+    if (!dir) {
+      stderr.write(FS.notFound('rm', arg));
+    } else if (typeof(dir) !== 'object'){
+      stderr.write(FS.error('rm', arg, 'Not a directory'));
+    } else if (typeof(dir[fileName]) === 'undefined') {
+      stderr.write(FS.notFound('rm', arg));
+    } else if (typeof(dir[fileName]) === 'object') {
+      stderr.write(FS.error('rm', arg, 'is a directory'));
+    } else {
+      delete dir[fileName];
+    }
+  });
+
+  FS.writeFS();
+  next();
+}
